41133be3e91653c107288de14fcad67b808b127b
masofeng <masofeng@126.com>
2015-09-10 16:50:14 +0800

    微信登录CI版
    基本完成登录功能

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends MY_Controller {
+
+
+	public function index()
+	{
+	   header("Content-Type: text/html; charset=UTF-8");        
+       var_dump($_SESSION);
+	}
+}
